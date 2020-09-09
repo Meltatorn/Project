3a331310862613a33809411c41cf4e325d487b7e
Oleg <Olegsb90@mail.ru>
2020-09-09 15:36:10 +0300

    feat: fixed block media slides

--- js/main.js
+++ js/main.js
@@ -17,9 +17,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 $(document).ready(function () {
   var modal = $('.modal'),
-      modalBtn = $('[data-toggle=modal]'),
+      modalBtn = $('[data-toggle="modal"]'),
       closeBtn = $('.modal__close');
-     
 
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
@@ -29,23 +28,23 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
 
-    var mySwiper = new Swiper ('.swiper-container', {
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-
-    var next = $('.swiper-button-next');
-    var prev = $('.swiper-button-prev');
-    var bullets = $('.swiper-pagination');
-
-    next.css('left', prev.width() + 10 + bullets.width() + 10);
-    bullets.css('left', prev.width() + 10);
-
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+  
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left', prev.width() + 10);
+  
 });
